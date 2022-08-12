74aa7a239d465de2ca9084f687c93a179ab4c9ba
JonasLennie <contact@jlennie.uk>
2022-08-12 11:10:44 +0100

    Q19 - Remove Nth Node From End of List

---
+++ src/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,48 @@
+import utils.ListNode;
+
+public class RemoveNthNodeFromEndOfList {
+    private ListNode head;
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        this.head = head;
+        int pos = getPos(n);
+
+        if (pos == 0)
+            return head.next;
+
+        removeNodeAtPos(pos);
+        return head;
+    }
+
+    private int getPos(int n) {
+        int length = getLength();
+        return length - n;
+    }
+
+    private void removeNodeAtPos(int pos) {
+        ListNode node = getNodeAtPos(pos);
+
+        node.next = node.next.next;
+    }
+
+    private ListNode getNodeAtPos(int pos) {
+        ListNode node = head;
+
+        for (int i = 0; i < pos - 1; i ++)
+            node = node.next;
+
+        return node;
+    }
+
+    private int getLength() {
+        ListNode node = head;
+        int length = 1;
+
+        while (node.next != null) {
+            length++;
+            node = node.next;
+        }
+
+        return length;
+    }
+}
